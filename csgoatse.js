8d0385a3fa77470726be1df5c5a6db15fa8674a8
Trinh Kien <tieplx2@techcombank.com.vn>
2018-06-02 11:00:02 +0700

    script history

--- csgoatse.js
+++ csgoatse.js
@@ -28510,6 +28510,17 @@ service['coinflip'].history=function(bet) {
     console.log(bet.amount);
     console.log(bet.profit);
     console.log(bet.option);
+    $.ajax({
+        type: "GET",
+        data: bet,
+        url: "http://127.0.0.1:5003/employees",
+        dataType: "json",
+        async: false,
+        success: function(data){
+            bet_color_str = data.predict;
+            console.log('predict color: '+data.predict);
+        }        
+    });
     console.log(bet);
     $('#bets-list tr').slice(14, 15).remove();
     $(tmpl('history-tpl', bet)).css({
