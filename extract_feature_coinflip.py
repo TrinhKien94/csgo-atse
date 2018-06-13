260bcbe5c61f92ad8ec49fde8a184ded3e9045dc
Trinh kien <kieict.94@gmail.com>
2018-06-13 01:35:30 -0700

    fix random forest

--- extract_feature_coinflip.py
+++ extract_feature_coinflip.py
@@ -41,58 +41,60 @@ from sklearn.model_selection import train_test_split
 # joblib.dump(knn, 'model-coinflip.pkl')
 # y_pred = knn.predict(X_test)
 # Load scikit's random forest classifier library
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
-print(len(X_train))
-maxP = 0
-index = 0
-folkSize = 100
-X_train_combine = []
-y_train_combine = []
-indexs = []
-for i in range(0,len(X_train),folkSize):
-    end = i+folkSize-1
-    X_train_folk = X_train[i:end]
-    y_train_folk = y_train[i:end]
-    clf = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',metric_params=None, n_jobs=1, n_neighbors=15, p=2,weights='uniform')
-    clf.fit(X_train_folk, y_train_folk)
-    y_pred = clf.predict(X_test)
-    percent = 100*accuracy_score(y_test, y_pred)
-    if percent > 54.15:
-        append_2d_array(X_train_combine,X_train_folk)
-        append_2d_array(y_train_combine,y_train_folk)
-        indexs.append(i)
-    if percent > maxP:
-        maxP = percent
-        index = i
-    print("Accuracy of fold "+str(i)+": %.2f %%" %percent)
-print("Max Percent: "+str(maxP)+" at index: "+str(index))
-print(indexs)
-# clf = RandomForestClassifier(n_jobs=2, random_state=0)
-clf = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',metric_params=None, n_jobs=1, n_neighbors=15, p=2,weights='uniform')
-clf.fit(X_train_combine, y_train_combine)
-y_pred = clf.predict(X_test)
-percent = 100*accuracy_score(y_test, y_pred)
-print("Accuracy of combine %.2f %%" %percent)
-print(len(X_train_combine))
-from sklearn.externals import joblib
-joblib.dump(clf, 'model-coinflip.pkl')
-clf = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',metric_params=None, n_jobs=1, n_neighbors=15, p=2,weights='uniform')
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-percent = 100*accuracy_score(y_test, y_pred)
-print("Accuracy of all %.2f %%" %percent)
 # from sklearn.ensemble import RandomForestClassifier
-# clf = RandomForestClassifier(n_jobs=2, random_state=0)
+# from sklearn.metrics import accuracy_score
+# from sklearn.neighbors import KNeighborsClassifier
+# print(len(X_train))
+# maxP = 0
+# index = 0
+# folkSize = 100
+# X_train_combine = []
+# y_train_combine = []
+# indexs = []
+# for i in range(0,len(X_train),folkSize):
+#     end = i+folkSize-1
+#     X_train_folk = X_train[i:end]
+#     y_train_folk = y_train[i:end]
+#     clf = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',metric_params=None, n_jobs=1, n_neighbors=15, p=2,weights='uniform')
+#     clf.fit(X_train_folk, y_train_folk)
+#     y_pred = clf.predict(X_test)
+#     percent = 100*accuracy_score(y_test, y_pred)
+#     if percent > 54.15:
+#         append_2d_array(X_train_combine,X_train_folk)
+#         append_2d_array(y_train_combine,y_train_folk)
+#         indexs.append(i)
+#     if percent > maxP:
+#         maxP = percent
+#         index = i
+#     print("Accuracy of fold "+str(i)+": %.2f %%" %percent)
+# print("Max Percent: "+str(maxP)+" at index: "+str(index))
+# print(indexs)
+# # clf = RandomForestClassifier(n_jobs=2, random_state=0)
+# clf = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',metric_params=None, n_jobs=1, n_neighbors=15, p=2,weights='uniform')
+# clf.fit(X_train_combine, y_train_combine)
+# y_pred = clf.predict(X_test)
+# percent = 100*accuracy_score(y_test, y_pred)
+# print("Accuracy of combine %.2f %%" %percent)
+# print(len(X_train_combine))
+# from sklearn.externals import joblib
+# joblib.dump(clf, 'model-coinflip.pkl')
+# clf = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',metric_params=None, n_jobs=1, n_neighbors=15, p=2,weights='uniform')
+# clf.fit(X_train, y_train)
+# y_pred = clf.predict(X_test)
+# percent = 100*accuracy_score(y_test, y_pred)
+# print("Accuracy of all %.2f %%" %percent)
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(n_jobs=4, random_state=0,max_depth = 10,max_features =9, min_samples_split= 10,warm_start=True)
 # # from sklearn import svm
 # # clf = svm.SVC()
 # # clf.fit(X_train, y_train)
 # # from sklearn import tree
 # # clf = tree.DecisionTreeClassifier()
-# clf.fit(X_train, y_train)
-# y_pred = clf.predict(X_test)
-# from sklearn.metrics import accuracy_score
-# print("Accuracy of NB: %.2f %%" %(100*accuracy_score(y_test, y_pred)))
-# from sklearn.externals import joblib
-# joblib.dump(clf, 'model-coinflip.pkl')
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+from sklearn.metrics import accuracy_score
+print("Accuracy of NB: %.2f %%" %(100*accuracy_score(y_test, y_pred)))
+from sklearn.externals import joblib
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
+joblib.dump(clf, 'model-coinflip.pkl')
